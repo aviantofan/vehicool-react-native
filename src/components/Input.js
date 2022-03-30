fd1af7958843e90009056fca5ba5b768879132cd
eexxprd <aviantofan@gmail.com>
2022-03-31 00:00:05 +0700

    feat: add init

---
+++ src/components/Input.js
@@ -0,0 +1,24 @@
+import { TextInput, SafeAreaView, StyleSheet } from 'react-native'
+import React from 'react'
+
+const Input = ({ placeholder }) => {
+  return (
+    <SafeAreaView>
+      <TextInput placeholder={placeholder} placeholderTextColor='#FFFF' style={styles.input} />
+    </SafeAreaView>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 12,
+    color: 'white',
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: 'rgba(255, 255, 255,0.5)',
+    height: 60,
+  },
+})
+
+export default Input
