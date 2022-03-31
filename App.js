5e6614523b683db08a4d1f466fb025fb87d86855
eexxprd <aviantofan@gmail.com>
2022-04-01 01:54:19 +0700

    feat: fix error

--- App.js
+++ App.js
@@ -1,34 +1,12 @@
-import React from 'react';
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
-import { auth } from './src/image/index';
+import React from 'react'
+import Login from './src/screens/Login'
+// import { NavigationContainer } from '@react-navigation/native'
+// import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={auth} resizeMode="cover" style={styles.img}>
-        <Text style={styles.text}>LET’S EXPLORE THE WORLD</Text>
-      </ImageBackground>
-    </View>
-  );
-};
+    <Login />
+  )
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  img: {
-    flex: 1,
-  },
-  text: {
-    color: 'white',
-    fontSize: 36,
-    fontFamily: 'Roboto',
-    lineHeight: 42,
-    fontWeight: '900',
-    textAlign: 'justify',
-    marginVertical: 10,
-    marginHorizontal: 25,
-  },
-});
-
-export default App;
+export default App
