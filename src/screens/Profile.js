b7665457481018c8ab5c75e8c45d5aa73e3b091e
eexxprd <aviantofan@gmail.com>
2022-04-03 14:10:25 +0700

    feat: add update screen profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,12 +1,84 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { Image, Text } from 'native-base';
+import FaIcon from 'react-native-vector-icons/FontAwesome';
+import Button from '../components/Button';
 
-const Profile = () => {
+const Profile = ({ navigation }) => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.main}>
+      <View style={styles.header}>
+        <Image
+          size={69}
+          resizeMode={'contain'}
+          borderRadius={200}
+          source={require('../assets/ava.png')}
+          alt="Profile Pic"
+        />
+        <Text bold fontSize="2xl" style={styles.name}>
+          Samantha Doe
+        </Text>
+      </View>
+      <View style={styles.container}>
+        <View>
+          <TouchableOpacity style={styles.linkItem}>
+            <Text fontSize="2xl">Your favourites</Text>
+            <FaIcon name="chevron-right" size={25} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.linkItem}>
+            <Text fontSize="2xl">FAQ</Text>
+            <FaIcon name="chevron-right" size={25} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.linkItem}>
+            <Text fontSize="2xl">Help</Text>
+            <FaIcon name="chevron-right" size={25} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.linkItem}
+            onPress={() => navigation.navigate('UpdateProfile')}>
+            <Text fontSize="2xl">Update profile</Text>
+            <FaIcon name="chevron-right" size={25} />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Button color="primary">Log out</Button>
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: '#fff',
+    height: '100%',
+  },
+  container: {
+    padding: 20,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '80%',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    elevation: 19,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  name: {
+    marginLeft: 30,
+  },
+  list: {
+    paddingVertical: 20,
+  },
+  linkItem: {
+    marginVertical: 25,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
 
 export default Profile
--- src/screens/UpdateProfile.js
+++ src/screens/UpdateProfile.js
@@ -1,12 +1,143 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+import React from 'react';
+import { Text, Image, Center, Radio, Stack } from 'native-base';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Button from '../components/Button';
 
-const UpdateProfile = () => {
+const UpdateProfile = ({ navigation: { goBack } }) => {
+  const dataInput = [
+    { label: 'Name', value: 'Samantha Doe' },
+    { label: 'Email Address', value: 'samanthadoe17@gmail.com', type: 'email-address' },
+    { label: 'Phone Number', value: '+62 81348287878', type: 'name-phone-pad' },
+    { label: 'Date of Birth', value: 'December 21th 1998' },
+    { label: 'Delivery Address', value: 'Iskandar Street Block A Number 102' },
+  ];
+
   return (
     <View>
-      <Text>UpdateProfile</Text>
+      <TouchableOpacity style={styles.back} onPress={() => goBack()}>
+        <EntypoIcon
+          name="chevron-left"
+          color="black"
+          size={35}
+          style={styles.icon}
+        />
+        <Text fontSize={20} bold style={styles.textBack}>
+          Update Profile
+        </Text>
+      </TouchableOpacity>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.wrapper}>
+          <View style={styles.profilePict}>
+            <Center>
+              <Image
+                size={99}
+                resizeMode={'contain'}
+                borderRadius={200}
+                source={require('../assets/ava.png')}
+                alt="Profile Pic"
+              />
+            </Center>
+            <View style={styles.iconEdit}>
+              <MaterialIcon
+                color="white"
+                name="pencil-outline"
+                style={styles.iconPen}
+                size={21}
+              />
+            </View>
+          </View>
+          <View style={styles.radioGrup}>
+            <Radio.Group defaultValue="1" name="myRadioGroup" accessibilityLabel="favorite colorscheme">
+              <Stack
+                direction={{ base: 'row' }}
+                alignItems="center"
+                space={4}
+                w="75%"
+                maxW="300px">
+                <Radio colorScheme='purple' value="1" my={1}>
+                  <Text style={styles.textRadio}>Female</Text>
+                </Radio>
+                <Radio colorScheme='purple' value="2" my={1}>
+                  <Text style={styles.textRadio}>Male</Text>
+                </Radio>
+              </Stack>
+            </Radio.Group>
+          </View>
+          {dataInput.map((data, index) => (
+            <View key={index}>
+              <Text style={styles.label}>{data.label}:</Text>
+              <TextInput defaultValue={data.value} style={styles.input} />
+            </View>
+          ))}
+          <View style={styles.button}>
+            <Button color="primary">Save change</Button>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: 20,
+  },
+  back: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  icon: {
+    fontWeight: 'bold',
+  },
+  textBack: {},
+  profilePict: {
+    marginTop: 10,
+    justifyContent: 'center',
+    position: 'relative',
+  },
+  iconEdit: {
+    position: 'absolute',
+    alignSelf: 'center',
+    bottom: 0,
+    right: 140,
+    backgroundColor: '#5C527F',
+    padding: 9,
+    borderRadius: 50,
+  },
+  radioGrup: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  textRadio: {
+    marginLeft: 8,
+  },
+  label: {
+    color: 'gray',
+    marginTop: 20,
+    marginBottom: 15,
+  },
+  input: {
+    height: 50,
+    color: 'black',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    paddingHorizontal: 15,
+  },
+  button: {
+    marginBottom: 80,
+    marginTop: 40,
+  },
+});
 
 export default UpdateProfile
