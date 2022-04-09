7ad6982f24daaa79d50d9b629a2007807d251b11
eexxprd <aviantofan@gmail.com>
2022-04-09 12:36:12 +0700

    feat: add init redux popular

---
+++ src/redux/reducers/popular.js
@@ -0,0 +1,21 @@
+const popularState = {
+  popularVehicle: {},
+  isLoading: false,
+  err: false,
+  errMsg: '',
+  successMsg: null,
+};
+
+const popular = (state = popularState, action) => {
+  switch (action.type) {
+    case 'GET_POPULAR': {
+      state.popularVehicle = action.payload.results;
+      return { ...state };
+    }
+    default: {
+      return { ...state };
+    }
+  }
+};
+
+export default popular;
