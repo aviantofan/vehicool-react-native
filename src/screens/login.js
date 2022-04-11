a77f6596d4b42b69e1d22cb89a82b0ff5cbdf945
eexxprd <aviantofan@gmail.com>
2022-04-12 06:40:56 +0700

    feat: add secure text in password

--- src/screens/login.js
+++ src/screens/login.js
@@ -58,6 +58,7 @@ const Login = () => {
               placeholder='Password'
               onChangeText={setPassword}
               value={password}
+              secureTextEntry={true}
             />
             <TouchableOpacity>
               <Text style={styles.forgot} onPress={() => {
