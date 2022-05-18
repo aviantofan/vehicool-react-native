a004fa210abd236dd1e56e65b0ee083b0f73336e
eexxprd <aviantofan@gmail.com>
2022-05-18 23:01:42 +0700

    feat: history navigate

--- src/navigations/HistoryNav.js
+++ src/navigations/HistoryNav.js
@@ -4,14 +4,16 @@ import History from '../screens/History'
 import ChatList from '../screens/ChatList'
 
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { useSelector } from 'react-redux'
 
 const HistoryTab = createMaterialTopTabNavigator()
 
 const HistoryNav = () => {
+  const { auth } = useSelector(state => state)
   return (
     <HistoryTab.Navigator >
       <HistoryTab.Screen name='Chat' component={ChatList} />
-      <HistoryTab.Screen name='History Order' component={History} />
+      {auth.userData?.role === 'user' && <HistoryTab.Screen name='History Order' component={History} />}
     </HistoryTab.Navigator>
   )
 }
